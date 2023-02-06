750cdd1137eb7be38f314bcafeb6e785116f7cd6
GodOfPro <1387ilia@gmail.com>
2023-02-06 15:14:20 +0330

    Added command

--- src/main/java/com/prind/ctf/CTF.java
+++ src/main/java/com/prind/ctf/CTF.java
@@ -1,5 +1,6 @@
 package com.prind.ctf;
 
+import com.prind.ctf.commands.CommandManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class CTF extends JavaPlugin {
@@ -7,7 +8,7 @@ public final class CTF extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        new CommandManager(this);
     }
 
     @Override
