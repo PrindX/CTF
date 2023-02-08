e43ce61786d3fa87bf7a0cc66f4b8c38ec5b4a6d
GodOfPro <1387ilia@gmail.com>
2023-02-08 20:18:39 +0330

    Actually save the data

--- src/main/java/com/prind/ctf/stats/StatsListener.java
+++ src/main/java/com/prind/ctf/stats/StatsListener.java
@@ -1,11 +1,14 @@
 package com.prind.ctf.stats;
 
 import com.prind.ctf.CTF;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
 import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 public class StatsListener implements Listener {
 
@@ -25,6 +28,14 @@ public class StatsListener implements Listener {
             // Prevent memory leak if login fails
             CTF.getInstance().getStatsManager().remove(event.getPlayer().getUniqueId());
         }
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        Player player = event.getPlayer();
+        PlayerStats playerStats = CTF.getInstance().getStatsManager().get(player.getUniqueId());
+        if (playerStats == null) return;
+        Bukkit.getScheduler().runTaskAsynchronously(CTF.getInstance(), () -> CTF.getInstance().getRemoteDatabase().saveStats(playerStats));
     }
 
 }
