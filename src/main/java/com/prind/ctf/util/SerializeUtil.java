e3ebf7ba37b394bfb31ae7664287f578313cd7b9
GodOfPro <1387ilia@gmail.com>
2023-02-09 18:36:36 +0330

    Update SerializeUtil.java

--- src/main/java/com/prind/ctf/util/SerializeUtil.java
+++ src/main/java/com/prind/ctf/util/SerializeUtil.java
@@ -1,6 +1,7 @@
 package com.prind.ctf.util;
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 
 public class SerializeUtil {
@@ -11,15 +12,15 @@ public class SerializeUtil {
         oos.writeObject(arrayList);
         oos.close();
 
-        return baos.toString("ISO-8859-1");
+        return baos.toString(StandardCharsets.ISO_8859_1);
     }
 
     public static ArrayList<?> deserializeArrayList(String str) throws IOException, ClassNotFoundException {
-        byte[] data = str.getBytes("ISO-8859-1");
+        byte[] data = str.getBytes(StandardCharsets.ISO_8859_1);
         ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(data));
         ArrayList<?> newList = (ArrayList<?>) ois.readObject();
         ois.close();
         return newList;
     }
-    
+
 }
