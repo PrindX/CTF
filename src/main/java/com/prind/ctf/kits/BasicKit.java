7c880f9294428ed3d55501075d129642ea9a7d5b
GodOfPro <1387ilia@gmail.com>
2023-02-12 19:28:42 +0330

    Finished Sword Of Death

--- src/main/java/com/prind/ctf/kits/BasicKit.java
+++ src/main/java/com/prind/ctf/kits/BasicKit.java
@@ -7,6 +7,7 @@ import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.inventory.meta.ItemMeta;
 
 public class BasicKit implements Kit {
     @Override
@@ -34,14 +35,7 @@ public class BasicKit implements Kit {
         PlayerInventory inv = player.getInventory();
         inv.clear();
 
-        inv.setArmorContents(new ItemStack[]
-                {
-                     new ItemStack(Material.IRON_BOOTS),
-                     new ItemStack(Material.IRON_LEGGINGS),
-                     new ItemStack(Material.IRON_CHESTPLATE),
-                     new ItemStack(Material.IRON_HELMET)
-                }
-        );
+        inv.setArmorContents(getArmorContents());
 
         inv.setItem(0, ItemBuilder.getCustomItem(
                 Material.DIAMOND_SWORD,
@@ -49,10 +43,40 @@ public class BasicKit implements Kit {
                 "Sword Of Death",
                 "Slay your enemies\nand get a reward\nfor each kill",
                 "Strike The Rage",
-                "Spawns a lighting bolt where you look at\nand damage everyone in\na 4 block radius"
+                "Spawns a lighting bolt near every\nplayer and damages them\nin a 4 block radius"
         ));
         inv.setItem(1, new ItemStack(Material.BOW));
         inv.setItem(2, new ItemStack(Material.ARROW, 16));
         inv.setItem(3, new ItemStack(Material.GOLDEN_APPLE, 6));
+    }
+
+    private ItemStack[] getArmorContents() {
+        ItemStack boots = new ItemStack(Material.IRON_BOOTS);
+        ItemStack leggings = new ItemStack(Material.IRON_LEGGINGS);
+        ItemStack chestplate = new ItemStack(Material.IRON_CHESTPLATE);
+        ItemStack helmet = new ItemStack(Material.IRON_HELMET);
+
+        ItemMeta bootsMeta = boots.getItemMeta();
+        bootsMeta.setUnbreakable(true);
+        boots.setItemMeta(bootsMeta);
+
+        ItemMeta leggingsMeta = boots.getItemMeta();
+        leggingsMeta.setUnbreakable(true);
+        leggings.setItemMeta(leggingsMeta);
+
+        ItemMeta chestplateMeta = boots.getItemMeta();
+        chestplateMeta.setUnbreakable(true);
+        chestplate.setItemMeta(chestplateMeta);
+
+        ItemMeta helmetMeta = boots.getItemMeta();
+        helmetMeta.setUnbreakable(true);
+        helmet.setItemMeta(helmetMeta);
+
+        return new ItemStack[] {
+          boots,
+          leggings,
+          chestplate,
+          helmet
+        };
     }
 }
--- src/main/java/com/prind/ctf/kits/listeners/SwordOfDeathListener.java
+++ src/main/java/com/prind/ctf/kits/listeners/SwordOfDeathListener.java
@@ -1,15 +1,27 @@
 package com.prind.ctf.kits.listeners;
 
+import com.prind.ctf.CTF;
 import com.prind.ctf.kits.enums.ItemEnum;
 import com.prind.ctf.kits.utils.ItemBuilder;
 import de.tr7zw.changeme.nbtapi.NBTItem;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.TextColor;
+import org.bukkit.World;
+import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 public class SwordOfDeathListener implements Listener {
+
+    private Map<UUID, Integer> cooldown = new HashMap<>();
 
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent event) {
@@ -18,6 +30,37 @@ public class SwordOfDeathListener implements Listener {
         ItemStack item = event.getItem();
         if (!(ItemBuilder.isCustomItem(item, ItemEnum.SWORD_OF_DEATH))) return;
 
-        System.out.println("SWORD_OF_DEATH RIGHT CLICK");
+        Player player = event.getPlayer();
+        if (cooldown.containsKey(player.getUniqueId())) {
+            player.sendMessage(Component.text("You are currently in cooldown for " + cooldown.get(player.getUniqueId()) + " seconds").color(TextColor.color(255, 0, 0)));
+            return;
+        }
+        World world = player.getWorld();
+
+        for (Entity entity : player.getNearbyEntities(4, 2, 4)) {
+            if (entity instanceof LivingEntity livingEntity) {
+                if (entity.getUniqueId().equals(player.getUniqueId())) continue;
+                livingEntity.damage(5);
+                livingEntity.setVelocity(livingEntity.getLocation().toVector().subtract(player.getLocation().toVector()).multiply(0.6f));
+                world.strikeLightning(livingEntity.getLocation());
+            }
+        }
+
+        cooldown.put(player.getUniqueId(), 10); // Seconds
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                int current = cooldown.get(player.getUniqueId());
+                if (current > 0) {
+                    cooldown.put(player.getUniqueId(), current - 1);
+                }
+                if (current == 0) {
+                    cooldown.remove(player.getUniqueId());
+                    cancel();
+                }
+            }
+        }.runTaskTimer(CTF.getInstance(), 0L, 20L);
+
+        event.setCancelled(true);
     }
 }
